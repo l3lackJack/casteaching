d7862329cc9958da17f866e819f8bf685677fd84
l3lackJack <gabrielionuturs@iesebre.com>
2021-11-11 18:58:15 +0100

    Acabant Primer Test

--- tests/Feature/Videos/VideoTest.php
+++ tests/Feature/Videos/VideoTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Videos;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -9,14 +10,24 @@ use Tests\TestCase;
 class VideoTest extends TestCase
 {
     /**
-     * A basic feature test example.
-     *
-     * @return void
+     * @test
      */
-    public function test_example()
+    public function users_canv_view_videos()
     {
-        $response = $this->get('/');
-
+      $video = Video::create([
+            'title'=> 'Ubuntu 101',
+            'description'=> '# Here Description',
+            'url' => 'https://youtu.be/w8j07_DBl_I',
+            'published_at'=> Carbon::parse('December 13'),
+            'completed'=> false,
+            'previous'=> null,
+            'next'=> null,
+            'series_id'=> 1
+        ]);
+        $response = $this->get('/videos/' .$video->id);
         $response->assertStatus(200);
+        $response->assertSee('Ubuntu 101');
+        $response->assertSee('Here Description');
+        $response->assertSee('December 13');
     }
 }
