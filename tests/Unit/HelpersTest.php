845cb0f83c5bbff06d03483cf3d4bd16991fb67e
l3lackJack <gabrielionuturs@iesebre.com>
2021-11-16 18:35:23 +0100

    107:13:05

--- tests/Unit/HelpersTest.php
+++ tests/Unit/HelpersTest.php
@@ -2,17 +2,34 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Hash;
 use PHPUnit\Framework\TestCase;
 
 class HelpersTest extends TestCase
 {
+    use RefreshDatabase;
     /**
-     * A basic unit test example.
      *
-     * @return void
+     * @test
      */
     public function create_default_user()
     {
-        $this->assertTrue(true);
+        create_default_user();
+        $this->assertDabaseCount('users',1);
+        $this->assertDatabaseHas('users',[
+            'email'=> config('casteaching.default.user.email'),
+        ]);
+        $this->assertDatabaseHas('users',[
+            'name'=> config('casteaching.default.user.name'),
+        ]);
+
+        $user = User::find(1);
+        $this->assertNotNull($user);
+        $this->assertEquals(config('casteaching.default_user.email'), $user->email);
+        $this->assertEquals(config('casteaching.default_user.name'), $user->name);
+
+        $this->assertTrue(Hash::check(config('casteaching.default_user.name'), $user->password));
+
     }
 }
