2fb1286510a21a3918007191af2bacc6e7019a08
l3lackJack <gabrielionuturs@iesebre.com>
2021-11-16 16:18:55 +0100

    WIP

---
+++ config/casteaching.php
@@ -0,0 +1,9 @@
+<?php
+
+return[
+    'default_user'=>[
+        'name'=> env('DEFAULT_USER_NAME','casteaching'),
+        'email'=> env('DEFAULT_USER_EMAIL','casteaching@gmail.com'),
+        'password'=> env('DEFAULT_USER_PASSWORD',12345678)
+]
+];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Video;
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +18,27 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        //Crear usuari propi pero tambe per a professor
+        User::create([
+            'name' => 'Gabriel',
+            'email'=>'gabrielurs@gmail.com',
+            'password'=> Hash::make((config('casteaching.user_default.password')))
+        ]);
+
+        User::create([
+            'name'=> env('DEFAULT_USER_NAME','casteaching'),
+            'email'=> env('DEFAULT_USER_EMAIL','casteaching@gmail.com'),
+            'password'=> env('DEFAULT_USER_PASSWORD',12345678)
+        ]);
+
+        Video::create([
+            'title'=> 'Ubuntu 101',
+            'description'=> '# Here Description',
+            'url' => 'https://youtu.be/w8j07_DBl_I',
+            'published_at'=> Carbon::parse('December 13'),
+            'previous'=> null,
+            'next'=> null,
+            'series_id'=> 1
+        ]);
     }
 }
