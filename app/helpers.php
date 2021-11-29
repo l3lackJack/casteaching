57e35e24a98b2020476b0781e3e1175d0e6f4505
l3lackJack <gabrielionuturs@iesebre.com>
2021-11-29 19:12:56 +0100

    107 Acabat

--- app/helpers.php
+++ app/helpers.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Models\Team;
 use App\Models\User;
 use App\Models\Video;
 use Carbon\Carbon;
@@ -7,11 +9,19 @@ use Illuminate\Support\Facades\Hash;
 if (!function_exists('create_default_user')){
 
 function create_default_user(){
-    User::create([
+   $user = User::create([
         'name'=> config('casteaching.default_user.name','casteaching'),
         'email'=> config('casteaching.default_user.email','casteaching@gmail.com'),
         'password' => Hash::make(config('casteaching.default_user.password', '12345678'))
     ]);
+    try {
+        Team::create([
+            'name'=> $user->name . "'s team",
+            'user_id'=> $user->id,
+            'personal_team'=> true
+        ]);
+    }catch (\Exception $exception){
+    }
 }
 }
 
