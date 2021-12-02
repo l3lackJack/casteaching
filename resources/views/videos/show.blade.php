3f0c401dc47c7f9d9797604d0b2193091afa431c
l3lackJack <gabrielionuturs@iesebre.com>
2021-12-02 16:13:39 +0100

    WIP

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -1,7 +1,7 @@
-<p>{{$video->title}}</p>
-<ul>
-    <li>{{ $video->description }}</li>
-    <li>Data: {{ $video->published_at}}</li>
-</ul>
-
-
+<x-casteaching-layout>
+    <p>{{$video->title}}</p>
+    <ul>
+        <li>{{ $video->description }}</li>
+        <li>Data: {{ $video->published_at}}</li>
+    </ul>
+</x-casteaching-layout>
