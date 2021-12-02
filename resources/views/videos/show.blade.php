37240a46161170f408ea44227dd2a72c3fc3e041
l3lackJack <gabrielionuturs@iesebre.com>
2021-12-02 18:57:13 +0100

    109 Acabat

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -1,7 +1,18 @@
 <x-casteaching-layout>
-    <p>{{$video->title}}</p>
-    <ul>
-        <li>{{ $video->description }}</li>
-        <li>Data: {{ $video->published_at}}</li>
-    </ul>
+    <div class="flex flex-col h-screen">
+        <iframe
+                class="md:p-3 lg:p-5 xl:p-10 xl:py-5 2xl:p-20 2xl:p-10 h-4/5"
+                height="600"
+                src="https://www.youtube.com/embed/qK311dtQr_4"
+                title="YouTube video player" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen>
+        </iframe>
+        <div class="p-4 lg:p-5 xl:p-10 xl:py-5 2xl:p-20 2xl:py-20">
+            {{$video->title}}
+        </div>
+        <div class="p-4 lg:p-5 xl:p-10 2xl:p-20">
+          {{ $video->description }}
+        </div>
+    </div>
 </x-casteaching-layout>
