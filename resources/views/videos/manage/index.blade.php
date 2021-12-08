b50b500409817128d8d304b86ff05679765295dd
l3lackJack <gabrielionuturs@iesebre.com>
2021-12-08 19:07:44 +0100

    WIP

--- resources/views/videos/manage/index.blade.php
+++ resources/views/videos/manage/index.blade.php
@@ -7,8 +7,10 @@
     </x-slot>
 
     <div class="flex flex-col mt-10">
+
         <div class="mx-auto sm:px-6 lg:px-8 w-full max-w-7xl">
             <x-status></x-status>
+
             @can('videos_manage_create')
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 mb-1 md:mb-2 lg:mb-4">
                     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
@@ -26,6 +28,7 @@
                                     @csrf
                                     <div class="shadow sm:rounded-md sm:overflow-hidden md:bg-white">
                                         <div class="px-4 py-5 space-y-6 sm:p-6">
+
                                             <div>
                                                 <label for="title" class="block text-sm font-medium text-gray-700">
                                                     Title
@@ -37,6 +40,7 @@
                                                     Titol curt del vídeo
                                                 </p>
                                             </div>
+
                                             <div>
                                                 <label for="description" class="block text-sm font-medium text-gray-700">
                                                     Description
@@ -48,6 +52,7 @@
                                                     Breu descripció del vídeo
                                                 </p>
                                             </div>
+
                                             <div class="grid grid-cols-3 gap-6">
                                                 <div class="col-span-3">
                                                     <label for="url" class="block text-sm font-medium text-gray-700">
@@ -61,6 +66,9 @@
                                                     </div>
                                                 </div>
                                             </div>
+
+
+
                                         </div>
                                         <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
                                             <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
@@ -73,68 +81,9 @@
                         </div>
                     </div>
                 </div>
-<<<<<<< HEAD
-            </div>
-
-        @endcan
-
-        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                    <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
-                        <h3 class="text-lg leading-6 font-medium text-gray-900">
-                            Videos
-                        </h3>
-                    </div>
-                    <table class="min-w-full divide-y divide-gray-200">
-                        <thead class="bg-gray-50">
-                        <tr>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Id
-                            </th>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Title
-                            </th>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                Description
-                            </th>
-                            <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                URL
-                            </th>
-                            <th scope="col" class="relative px-6 py-3">
-                                <span class="sr-only">Actions</span>
-                            </th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($videos as $video)
-                            <tr class="bg-white">
-                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                                    {{ $video->id }}
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    {{ $video->title }}
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    {{ $video->description }}
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    {{ $video->url }}
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                    <a href="/videos/{{$video->id}}" target="_blank" class="text-indigo-600 hover:text-indigo-900">Show</a>
-                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                    <form class="inline" action="/manage/videos/{{$video->id}}" method="POST">
-                                        @csrf
-                                        @method('DELETE')
-
-                                        <a href="/videos/{{$video->id}}" class="text-indigo-600 hover:text-indigo-900"
-                                           onclick="event.preventDefault();
-                                        this.closest('form').submit();">Delete</a>
-                                    </form>
-                                </td>
 
             @endcan
+
             <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
@@ -161,7 +110,6 @@
                                 <th scope="col" class="relative px-6 py-3">
                                     <span class="sr-only">Actions</span>
                                 </th>
->>>>>>> main
                             </tr>
                             </thead>
                             <tbody>
@@ -186,7 +134,10 @@
                                         <form class="inline" action="/manage/videos/{{$video->id}}" method="POST">
                                             @csrf
                                             @method('DELETE')
-                                            <a href="/videos/{{$video->id}}" class="text-indigo-600 hover:text-indigo-900" onclick="event.preventDefault(); this.closest('form').submit();">Delete</a>
+
+                                            <a href="/videos/{{$video->id}}" class="text-indigo-600 hover:text-indigo-900"
+                                               onclick="event.preventDefault();
+                                        this.closest('form').submit();">Delete</a>
                                         </form>
                                     </td>
                                 </tr>
@@ -216,4 +167,5 @@
             </div>
         </div>
     </div>
+
 </x-casteaching-layout>
