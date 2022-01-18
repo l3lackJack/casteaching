9cbc5a6eb86018c8b33db425f71278fe4e3d63fa
l3lackJack <gabrielionuturs@iesebre.com>
2022-01-18 17:46:01 +0100

    WIP

--- app/Listeners/SendVideoCreatedNotification.php
+++ app/Listeners/SendVideoCreatedNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Notification;
 
-class SendVideoCreatedNotification
+class SendVideoCreatedNotification implements ShouldQueue
 {
     /**
      * Handle the event.
