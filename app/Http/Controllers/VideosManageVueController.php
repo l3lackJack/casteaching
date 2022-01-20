bcf16041d5b94e7dfbc87b12a66bf8addc981635
l3lackJack <gabrielionuturs@iesebre.com>
2022-01-20 18:46:20 +0100

    elimnats metodes del controlador que no utilitzem

--- app/Http/Controllers/VideosManageVueController.php
+++ app/Http/Controllers/VideosManageVueController.php
@@ -4,10 +4,8 @@ namespace App\Http\Controllers;
 
 class VideosManageVueController extends Controller
 {
-
     public function index()
     {
         return view('videos.manage.vue.index');
     }
-
 }
