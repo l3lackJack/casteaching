92a559b1747b74b0f67e8a4c5a463007b17ad896
l3lackJack <gabrielionuturs@iesebre.com>
2022-02-17 17:29:23 +0100

    Acabo 134 amb errors

---
+++ tests/Feature/GithubDriverMock.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Tests\Feature;
+
+class GithubDriverMock
+{
+
+    const ID = 4015406;
+    const NAME = "Sergi Tur Badenas";
+    const NICKNAME = "acacha";
+    const AVATAR = "TODO";
+    const EMAIL = "sergiturbadenas@gmail.com";
+//    const EMAIL = "sergiturbadenas@gmail.com";
+    const TOKEN = "TOKEN_HERE";
+    const REFRESH_TOKEN = null;
+
+    public $user;
+
+    /**
+     * @param $user
+     */
+    public function __construct($user = null)
+    {
+        if ($user) {
+            $this->user = $user;
+            return;
+        }
+        $this->setDefaultUser();
+    }
+
+
+    public function user()
+    {
+        return $this->user;
+    }
+
+    private function setDefaultUser()
+    {
+        $user = new \stdClass();
+        $user->id = GithubDriverMock::ID;
+        $user->email = GithubDriverMock::EMAIL;
+        $user->name = GithubDriverMock::NAME;
+        $user->nickname = GithubDriverMock::NICKNAME;
+        $user->token = GithubDriverMock::TOKEN;
+        $user->refreshToken = GithubDriverMock::REFRESH_TOKEN;
+        $this->user = $user;
+    }
+}
