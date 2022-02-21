c90e87221110c48ac60340bafb7c2fe52fff829d
l3lackJack <gabrielionuturs@iesebre.com>
2022-02-21 19:05:22 +0100

    WIP

--- app/Console/Commands/YoutubeImportPlaylist.php
+++ app/Console/Commands/YoutubeImportPlaylist.php
@@ -8,9 +8,7 @@ use App\Models\User;
 use App\Models\Video;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
-use Illuminate\Http\File;
 use Illuminate\Http\FileHelpers;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class YoutubeImportPlaylist extends Command
@@ -48,34 +46,15 @@ class YoutubeImportPlaylist extends Command
      */
     public function handle()
     {
-        // CANAL ACACHA_DEV
-        //https://www.youtube.com/channel/UColShZx2cvqR28k_wOVPRUg
-        // Canal: UColShZx2cvqR28k_wOVPRUg
-        // Playlist
-        // TDD https://www.youtube.com/playlist?list=PLyasg1A0hpk07HA0VCApd4AGd3Xm45LQv
-        // TDD playlist: PLyasg1A0hpk07HA0VCApd4AGd3Xm45LQv
-        //    $video = Youtube::getVideoInfo('rie-hPVJ7Sw');
-        //    dd($video);
-
-        // Get playlist by ID, return an STD PHP object
         $playlist = Youtube::getPlaylistById($this->argument('playlistId'));
         $this->info('Serie name: ' . $playlist->snippet->title);
         $this->info('Creating serie...');
-//            dd($playlist->snippet);
-
-//            $path = Storage::download($playlist->snippet->thumbnails->maxres->url);
 
         $extension = pathinfo(storage_path(($playlist->snippet->thumbnails->maxres->url), PATHINFO_EXTENSION))['extension'];
         $filename = Str::slug($playlist->snippet->title).'.'.$extension;
         $path = 'app/public/series/' . $filename;
         copy($playlist->snippet->thumbnails->maxres->url, storage_path($path));
 
-        //            dd(storage_path($path));
-//
-//        dd(Str::random(40));
-//            dd($path);
-//            dd($playlist->snippet->thumbnails->maxres->url);
-        //            https://i.ytimg.com/vi/ednlsVl-NHA/maxresdefault.jpg
 
         $serie = Serie::create([
             'title' => $playlist->snippet->title,
@@ -92,9 +71,6 @@ class YoutubeImportPlaylist extends Command
                 continue;
             }
             $this->info($item->snippet->title);
-            // resourceId": {#2327 //    +"kind": "youtube#video" //    +"videoId
-            // ednlsVl-NHA
-            // https://youtu.be/ednlsVl-NHA
             $video = Video::create([
                 'title' => $item->snippet->title,
                 'description' => $item->snippet->description,
